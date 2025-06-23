17b0ebb81a94b5fb37b824ffd0f4748bb20f565e
phamcaohung <phamcao77@gmail.com>
2025-06-23 11:18:05 +0700

    upload

---
+++ app/(root)/index.jsx
@@ -0,0 +1,87 @@
+import { useUser } from '@clerk/clerk-expo'
+import { useRouter } from 'expo-router'
+import { Alert, FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
+import { SignOutButton } from '@/components/SignOutButton'
+import { useTransactions } from '../../hook/useTransactions'
+import { useEffect } from 'react'
+import PageLoader from '../../components/PageLoader'
+import { styles } from '../../assets/styles/home.styles'
+import { Ionicons } from "@expo/vector-icons"
+import BalanceCard from '../../components/BalanceCard'
+import TransactionItem from '../../components/TransactionItem'
+
+
+export default function Page() {
+    const { user } = useUser()
+    const router = useRouter()
+    const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(user.id)
+
+    useEffect(() => {
+        loadData()
+    }, [loadData])
+
+    const handleDelete = (id) => {
+        Alert.alert("Delete Transaction", "Are you sure you want to delete this transaction ?", [
+            { text: "Cancel", style: "cancel" },
+            { text: "Delete", style: "destructive", onPress: () => deleteTransaction(id) }
+        ])
+    }
+    
+
+    if(isLoading) return <PageLoader />
+    
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                {/* HEADER */}
+                <View style={styles.header}>
+                    {/* LEFT */}
+                    <View style={styles.headerLeft}>
+                        <Image 
+                            source={require("../../assets/images/logo.png")}
+                            style={styles.headerLogo}
+                            resizeMode='contain'
+                        />
+                        <View style={styles.welcomeContainer}>
+                            <Text style={styles.welcomeText}>Welcome,</Text>
+                            <Text style={styles.usernameText}>
+                                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}    
+                            </Text>
+                        </View>
+                    </View>
+                    {/* RIGHT */}
+                    <View style={styles.headerRight}>
+                        <TouchableOpacity 
+                            style={styles.addButton} 
+                            onPress={() => router.push("/create")}
+                        >
+                            <Ionicons name='add' size={20} color="#FFF" />
+                            <Text style={styles.addButtonText}>Add</Text>
+                        </TouchableOpacity>
+                        <SignOutButton/>
+                    </View>
+                </View>
+
+
+                <BalanceCard summary={summary} />
+
+
+                <View style={styles.transactionsHeaderContainer}>
+                    <Text style={styles.sectionTitle}>
+                        Recent Transactions
+                    </Text>
+                </View>
+            </View>
+
+
+            <FlatList
+                style={styles.transactionsList}
+                contentContainerStyle={styles.transactionsListContent}
+                data={transactions}
+                renderItem={({item}) => (
+                    <TransactionItem item={item} onDelete={handleDelete} />
+                )}
+            />
+        </View>
+    )
+}
---
+++ constants/api.js
@@ -0,0 +1,2 @@
+// const API_URL = "https://wallet-5l3h.onrender.com/api"
+export const API_URL = "http://192.168.2.13:5001/api"
